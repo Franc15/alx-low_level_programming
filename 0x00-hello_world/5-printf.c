f2ea7636c7680cc70cc836fcb66f6c3aeecad0ef
Franc15 <f.kikulwe@alustudent.com>
2021-06-17 03:42:53 -0700

    Added new file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Francis Kikulwe
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints with proper grammar, but the 
+ *                 outcome is a piece of art
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
