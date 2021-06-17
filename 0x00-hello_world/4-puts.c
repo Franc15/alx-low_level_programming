261e4aac378182e28270954d1872d210f1500869
Franc15 <f.kikulwe@alustudent.com>
2021-06-17 03:37:14 -0700

    Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 
 #include <stdio.h>
 
-/*
+/**
  * main - Prints "Programming is like building a multilingual
  *                 puzzle, followed by a new line
  *
@@ -14,6 +14,6 @@
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
