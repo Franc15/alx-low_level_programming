2baded7580ff6ed616ce3463ccf18ad9a48a7236
Franc15 <f.kikulwe@alustudent.com>
2021-06-17 03:45:09 -0700

    changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,13 +7,13 @@
 
 /**
  * main - Prints with proper grammar, but the 
- *                 outcome is a piece of art
+ *                 outcome is a piece of art,
  *
  * Return: 0
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
