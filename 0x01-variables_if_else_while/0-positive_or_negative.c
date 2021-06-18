79b3f4fb887bc25ce14a0cd6a0e1ee8f2d16ff8c
Franc15 <f.kikulwe@alustudent.com>
2021-06-18 05:31:04 -0700

    Added the file 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,19 +4,21 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/** main Function - to generate random numbers and print relevant statements to the STDOUT */
+/** main Function - to generate random numbers and 
+ * print relevant statements to the STDOUT 
+ */
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-  if (n > 0)
-    printf("%d is positive", n);
-  else if (n < 0)
-    printf("%d is negative", n);
-  else
-    printf("%d is zero", n);
-  return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+return (0);
 }
