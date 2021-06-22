41036c3cec52bc2f74cc7fa7825f42d0285c4792
Franc15 <f.kikulwe@alustudent.com>
2021-06-22 08:28:19 -0700

    Added file 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,9 +3,24 @@
 * Auth: Francis Kikulwe
 */
 
-#include <holberton.h>
-int main (void)
+#include "holberton.h"
+
+/**
+* main - Prints "Holberton" followed by a new line
+*
+* Return: Always 0
+*/
+int main(void)
 {
-_putchar("Holberton");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
  return (0);
 }
