84b2fdf556dac0225e9ee61e4c0aa8d8301c18df
Franc15 <f.kikulwe@alustudent.com>
2021-06-22 11:04:10 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+/*
+* File: 1-alphabet.c
+* Auth: Francis Kikulwe
+*/
+
+#include "holberton.h"
+
+void print_alphabet(void)
+{
+char l;
+for(l='a';l<='z';l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
