6256518e6683f343b02ac9da8a8b1bddc5cd7634
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 07:55:07 -0700

    Updated files

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,11 @@
-/*
-* File: 0-isupper.c
-* Auth: Francis Kikulwe
-*/
-
 #include "holberton.h"
 
 /**
-*  _isupper - checks if character is uppercase or not
-*             returns 1 if uppercase and 0 if not
-*
-* Return: Always 0
+* _isupper - checks for uppercase character
+* @c: the character to be checked
+* Return: 1 if c is uppercase, 0 otherwise
 */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+return (c >= 'A' && c <= 'Z');
 }
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,20 +1,11 @@
-/*
-* File: 1-isdigit.c
-* Auth: Francis Kikulwe
-*/
-
 #include "holberton.h"
 
 /**
-* _isdigit - checks if an inout is a digit or not
-*           return 1 if digit and 0, otherwise
-*
-* Return: Always 0
+* _isdigit - checks for a digit (0 through 9)
+* @c: int to be checked
+* Return: 1 if c is a digit, 0 otherwise
 */
 int _isdigit(int c)
 {
-if(c >= '0' && c <= '9')
-return(1);
-else
-return(0);
+return (c >= '0' && c <= '9');
 }
