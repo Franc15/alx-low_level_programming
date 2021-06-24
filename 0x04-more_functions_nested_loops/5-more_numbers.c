fb1ae1fccd5e0e369af244b4328e156fdf4d7b8a
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 07:03:50 -0700

    Added file 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* more_numbers - print numbers 0-14 ten times
+* followed by new line
+*/
+void more_numbers(void)
+{
+int times = 10;
+while (times--)
+{
+int i;
+for (i = 0; i < 15; i++)
+_putchar(i + '0');
+_putchar('\n');
+}
+_putchar('\n');
+}
