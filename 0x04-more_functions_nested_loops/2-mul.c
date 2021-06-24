cd791f7ea1de379cd4269930c84b1da16e920d99
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 06:05:59 -0700

    Added file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/*
+* File: 2-mul.c
+* Auth: Francis Kikulwe
+*/
+
+#include "holberton.h"
+
+/**
+* mul - return the product of two integers
+*
+* Return: Always 0
+*/
+int mul(int a, int b)
+{
+return(a*b);
+}
