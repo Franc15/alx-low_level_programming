43ab643c4592a072d02924c307c22e7d1d9d50f4
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 07:23:59 -0700

    Added file 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.c"
+
+/**
+* print_line - print lines with response to arguments passed to it
+* followed by new line
+* @n: Number of line to be printed
+*/
+
+void print_line(int n)
+{
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+_putchar('_');
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
