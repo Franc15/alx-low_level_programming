5fc392f0c014f0b4cf421860c36ba0bb1c0f8c33
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 06:18:51 -0700

    Added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: Francis Kikulwe
+ */
+
+#include "holberton.h"
+
+void print_numbers(void)
+{
+for(int i = 0; i < 10; i++)
+_putchar(i);
+_putchar('\n');
+}
