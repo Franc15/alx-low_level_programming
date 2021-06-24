0354949cc36c8ee3ed5d4dcb8cc41c49c7f3d888
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 06:29:09 -0700

    Added 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,14 +6,14 @@
 #include "holberton.h"
 
 /**
-* print_numbers - prints the numbers 0-9
-* 
-* Return: Always 0
+* print_numbers - Prints the numbers from 0-9.
 */
 void print_numbers(void)
 {
-int i;
-for (i = 0; i <= 9; i++)
-_putchar((i%2) + '0');
+int num;
+
+for (num = 0; num <= 9; num++)
+_putchar((num % 10) + '0');
+
 _putchar('\n');
 }
