addb94d53af4e6b2bdca20d9b710d328a5eb253a
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 08:26:25 -0700

    Added the file 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "holberton.c"
+
+/**
+* main - checks if the number is positive or negative
+* Return: Always 0
+*/
+int main(void)
+{
+int i = 0;
+
+positive_or_negative(i);
+return(0);
+}
