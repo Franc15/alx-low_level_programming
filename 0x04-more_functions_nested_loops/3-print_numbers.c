5f2ae727ae7fcb129e38d7f69b757797c2063c51
Franc15 <f.kikulwe@alustudent.com>
2021-06-24 06:22:16 -0700

    Added 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,15 @@
 /*
- * File: 3-print_numbers.c
- * Auth: Francis Kikulwe
- */
+* File: 3-print_numbers.c
+* Auth: Francis Kikulwe
+*/
 
 #include "holberton.h"
 
+/**
+* print_numbers - prints the numbers 0-9
+* 
+* Return: Always 0
+*/
 void print_numbers(void)
 {
 for(int i = 0; i < 10; i++)
